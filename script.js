b677c15ac258678d957cda7511824da0320319f7
Johns189 <fkodeloftet@gmail.com>
2024-10-20 13:16:35 +0200

    added-JS-to-get-the-carusell-going

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello!');
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll("#spesific-button button");
+  const mainImage = this.doctype.getElementById("main-image");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const newImageSrc = button.getAttribute("data-image");
+      mainImage.src = newImageSrc;
+    });
+  });
+});
