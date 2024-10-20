d85d0e8b584f1786c257cadd94f32fb962563f0a
Johns189 <fkodeloftet@gmail.com>
2024-10-20 13:47:09 +0200

    fixed-an-error-in-JS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const buttons = document.querySelectorAll("#spesific-button button");
-  const mainImage = this.doctype.getElementById("main-image");
+  const mainImage = this.document.getElementById("main-image");
 
   buttons.forEach((button) => {
     button.addEventListener("click", function () {
